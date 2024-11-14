a5759d4e7c37eb5f3db36eb4a26a0e81de2a0e7b
DomesticSnail <philliptowler94@gmail.com>
2024-11-14 21:21:14 +0900

    first

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int score = 34; // 成績
+
+        // Use a ternary operator to decide the result
+        String result = (score >= 60) ? "合格です！" : "赤点です...";
+
+        // Display the result
+        System.out.println(result);
+    }
+}
